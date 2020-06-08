fa1255f813eac743e7d871c1949ccfd11caa9d63
aleg228 <66637921+aleg228@users.noreply.github.com>
2020-06-08 23:47:05 +0500

    Add files via upload
    2 урок

---
+++ 2_lesson/main.js
@@ -0,0 +1,16 @@
+let money = 15000;
+console.log(typeof money);
+let income= "работа";
+console.log(typeof income);
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = true;
+console.log(typeof deposit);
+let mission = '100000';
+let period = 8;
+
+console.log(addExpenses.length);
+console.log("Период равен" + period + "месяцев");
+console.log("Цель заработать" + mission + "рублей");
+
+let budgetDay = money/ 30;
+console.log(budgetDay);
