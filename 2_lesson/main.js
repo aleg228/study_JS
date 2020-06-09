98f89d4a84993056aa9e19778a2f15fc9b035295
aleg228 <66637921+aleg228@users.noreply.github.com>
2020-06-09 08:43:02 +0500

    Add files via upload
    добавил пару новых выводов

---
+++ 2_lesson/main.js
@@ -0,0 +1,18 @@
+let money = 15000;
+console.log(typeof money);
+let income= "работа";
+console.log(typeof income);
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+console.log(typeof deposit);
+let mission = '100000';
+let period = 8;
+
+console.log(addExpenses.length);
+console.log("Период равен" + period + "месяцев");
+console.log("Цель заработать" + mission + "рублей");
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let budgetDay = money/ 30;
+console.log(budgetDay);
